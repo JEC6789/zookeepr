3e3b2df54fea41afa3241cb7da49a8b158ba0b77
Jonathan Cushing <firex42069@gmail.com>
2022-05-02 23:40:25 -0400

    set up front-end

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { animals } = require("./data/animals.json");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -90,6 +91,22 @@ app.post("/api/animals", (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
